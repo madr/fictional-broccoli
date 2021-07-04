9adf5e98b46f6f9a1a6f80c38afe6cd8f809ad9d
Anders Ytterström <yttan@fastmail.se>
2021-07-04 16:26:38 +0200

    Add Express routes
     * /search will perform an API call to TMDB API.
     * /trending will list the top 100 most searched terms.
    
    Also, add a safe guard to avoid EADDRINUSE error for supertest.

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,38 @@ import "reflect-metadata";
 import express from "express";
 import { Request, Response } from "express";
 import { createConnection } from "typeorm";
+import { Search } from "./entity/Search";
+import { getTrending, logSearch } from "./search";
+import { SearchMovies } from "./api-client";
 
 require("dotenv").config();
 
 createConnection()
   .then((connection) => {
+    const repository = connection.getRepository(Search);
     const app = express();
     const PORT = process.env.EXPRESS_PORT;
 
     app.use(express.json());
 
-    app.get("/", async function (req: Request, res: Response) {
-      res.send("Hello, World!");
+    app.get("/search", async function (req: Request, res: Response) {
+      const term = <string>req.query.q;
+      if (!term) {
+        res.send("Please provide a search term. Example: ?q=terminator");
+      } else {
+        const movies = await SearchMovies(term);
+        logSearch(repository, term, movies.total_results);
+        return res.json(movies);
+      }
     });
 
-    app.listen(PORT);
+    app.get("/trending", async function (req: Request, res: Response) {
+      const searches = await getTrending(repository);
+      res.json(searches);
+    });
+
+    if (process.env.NODE_ENV !== "test") {
+      app.listen(PORT);
+    }
   })
   .catch((error) => console.log(error));
